d158cd77bba2e452238d54a7ca189dfd57f81fbb
junior <quicanomorenojunior21072004@gmail.com>
2025-09-28 15:42:50 -0500

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://junior-21-21.github.io/Taller-soldadura',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
